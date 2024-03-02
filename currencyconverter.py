30b253de46a97a30ab0e1a62684d82522ef0a42d
DESMYK <desmykofficial@gmail.com>
2024-03-02 11:25:19 +0300

    Desmond Done

---
+++ currencyconverter.py
@@ -0,0 +1,10 @@
+# Currency Converter Program
+
+# Get the US dollar amount from the user
+us_dollars = float(input("Enter the amount in USD: "))
+
+# Convert US dollars to Kenya SH
+kenya_sh = us_dollars * 150.50
+
+# Display the result
+print(f"{us_dollars} US dollars is equal to {kenya_sh} Kenya SH.")
--- hangman.py
+++ hangman.py
@@ -1,11 +1,23 @@
 # Hangman Game 
 import random 
+# imports counter to couunt number of occurrence of each letter in word
 from collections import Counter 
-  
-gamewords = '''dictionary love beer friends food shoes prince stellar greater
-adrenaline infinite program treason lawyer prayer forever ideas mongoose playboy'''
-  
+
+# defines a list of words to be used in the game  
+gamewords = ['dictionary', 'love', 'beer', 'friends', 'food', 'shoes', 'prince', 'stellar', 'greater',
+'adrenaline', 'infinite', 'program', 'treason', 'lawyer', 'prayer', 'forever', 'ideas', 'mongoose', 'playboy']
+
+# function that randomly selects a word from the list and
 def select_word(gamewords):
     return random.choice(gamewords)
 
+# prints randomly chosen word from the list
 print(select_word(gamewords))
+
+# an integer that represents number of remaining attempts
+# 6 representing (head + body +arms + legs)
+remaining_attempts = 6
+
+# displays all letters guessed by the user thar are in 'secret' word 
+guessed_letters = ""
+
